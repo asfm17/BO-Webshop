9fbd1e3c7b6a9a00d1648b2c19beb7fc419d310e
Akira <37931@ma-web.nl>
2024-06-27 22:31:44 +0200

    productinfo

--- js/main.js
+++ js/main.js
@@ -1,64 +1,112 @@
 let allProducts = document.getElementsByClassName("product");
 let filters = document.getElementsByClassName("filter");
 
-window.scrollTo(0,0);
+window.scrollTo(0, 0);
 
-setInterval(function(){
+setInterval(function () {
     document.getElementsByTagName("body")[0].style.overflow = "auto";
 }, 1500);
 
-for(let i =0; i < filters.length; i++){
-    filters[i].checked = true;
+for (let i = 0; i < filters.length; i++) {
+    filters[i].checked = false;
 }
 
 //accessoires filter
-let accesoiresFilter = document.getElementById("checkbox-accessoires");
-console.log("accesoiresFilter typeof: " + typeof(accesoiresFilter));
-accesoiresFilter.onchange = function(){
+let accessoiresFilter = document.getElementById("checkbox-accessoires");
+console.log(accessoiresFilter);
+accessoiresFilter.onchange = function () {
     console.log('change');
-    if(accesoiresFilter.checked === true){
-        for(let i =0; i < allProducts.length; i++){
-            if(allProducts[i].dataset.category === "accesoires"){
-        }       allProducts[i].style.display = "block";
+    if (accessoiresFilter.checked === true) {
+        for (let i = 0; i < allProducts.length; i++) {
+            console.log(allProducts[i].dataset.category);
+            if (allProducts[i].dataset.category === "accessoires") {
+                allProducts[i].style.display = "block";
+            }
+            else {
+                allProducts[i].style.display = "none";
+            }
+        }
     }
-}
+    else {
+        for (let i = 0; i < allProducts.length; i++) {
+            allProducts[i].style.display = "block";
+        }
+    }
 }
 
 //celestron filter
 let celestronFilter = document.getElementById("checkbox-celestron");
-console.log("celestronFilter typeof: " + typeof(celestronFilter));
-celestronFilter.onchange = function(){
-    console.log('change');
-    if(celestronFilter.checked === true){
-        for(let i =0; i < allProducts.length; i++){
-            if(allProducts[i].dataset.category === "celestron"){
-        }       allProducts[i].style.display = "block";
+console.log(celestronFilter);
+celestronFilter.onchange = function () {
+    console.log('change celestron');
+    if (celestronFilter.checked === true) {
+        for (let i = 0; i < allProducts.length; i++) {
+            if (allProducts[i].dataset.category === "celestron") {
+                allProducts[i].style.display = "block";
+            }
+            else {
+                allProducts[i].style.display = "none";
+            }
+        }
     }
-}
+    else {
+        for (let i = 0; i < allProducts.length; i++) {
+            allProducts[i].style.display = "block";
+        }
+    }
 }
 
 //meade filter
 let meadeFilter = document.getElementById("checkbox-meade");
-console.log("meadeFilter typeof: " + typeof(meadeFilter));
-meadeFilter.onchange = function(){
+console.log(meadeFilter);
+meadeFilter.onchange = function () {
     console.log('change');
-    if(meadeFilter.checked === true){
-        for(let i =0; i < allProducts.length; i++){
-            if(allProducts[i].dataset.category === "meade"){
-        }       allProducts[i].style.display = "block";
+    if (meadeFilter.checked === true) {
+        for (let i = 0; i < allProducts.length; i++) {
+            console.log(allProducts[i].dataset.category);
+            if (allProducts[i].dataset.category === "meade") {
+                allProducts[i].style.display = "block";
+            }
+            else {
+                allProducts[i].style.display = "none";
+            }
+        }
     }
-}
+    else {
+        for (let i = 0; i < allProducts.length; i++) {
+            allProducts[i].style.display = "block";
+        }
+    }
 }
 
 //orion filter
 let orionFilter = document.getElementById("checkbox-orion");
-console.log("orionFilter typeof: " + typeof(orionFilter));
-orionFilter.onchange = function(){
+console.log(orionFilter);
+orionFilter.onchange = function () {
     console.log('change');
-    if(orionFilter.checked === true){
-        for(let i =0; i < allProducts.length; i++){
-            if(allProducts[i].dataset.category === "orion"){
-        }       allProducts[i].style.display = "block";
+    if (orionFilter.checked === true) {
+        for (let i = 0; i < allProducts.length; i++) {
+            console.log(allProducts[i].dataset.category);
+            if (allProducts[i].dataset.category === "orion") {
+                allProducts[i].style.display = "block";
+            }
+            else {
+                allProducts[i].style.display = "none";
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < allProducts.length; i++) {
+            allProducts[i].style.display = "block";
+        }
     }
 }
-}
+
+function toggleText() {
+    var text = document.getElementById("demo");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
